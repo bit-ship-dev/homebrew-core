1bf26c0283a9021f5ea5395961d8c3e2265a5f26
Matus Poruben <matus.poruben@gmail.com>
2026-05-08 23:45:46 +0200

    test with nightly

--- Formula/bit-ship.rb
+++ Formula/bit-ship.rb
@@ -1,15 +1,16 @@
 class BitShip < Formula
   desc "Containerized deployment tool"
   homepage "https://www.bit-ship.dev"
-
-  version "1.0.0"
+  url "https://registry.npmjs.org/bit-ship/-/bit-ship-0.5.0-27c4463.tgz"
+  sha256 "REPLACE_WITH_SHA256"
+  version "0.5.0-27c4463"
   license "MIT"
 
   depends_on "node"
   depends_on "podman"
 
   def install
-    system "#{Formula["node"].opt_bin}/npm", "install", "-g", "bit-ship@latest"
+    system "#{Formula["node"].opt_bin}/npm", "install", "-g", "--prefix", prefix, "bit-ship"
   end
 
   test do
