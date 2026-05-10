6a4f737fdcffb5a3c51299692365fc38df5d2851
Matus Poruben <matus.poruben@gmail.com>
2026-05-10 02:20:32 +0200

    update version and add aliases

--- Formula/bit-ship.rb
+++ Formula/bit-ship.rb
@@ -2,7 +2,7 @@ class BitShip < Formula
   desc "Container tool"
   homepage "https://www.bit-ship.dev"
 
-  url "https://registry.npmjs.org/bit-ship/-/bit-ship-0.5.0-27c4463.tgz"
+  url "https://registry.npmjs.org/bit-ship/-/bit-ship-0.5.0-1eb2838.tgz"
   sha256 "0bac06268ad8b1be3de6bfc10db65ea798e3e4e761fcce5525e69b867f5ae701"
 
   version "0.5.0-27c4463"
@@ -13,6 +13,9 @@ class BitShip < Formula
 
   def install
     system "#{Formula["node"].opt_bin}/npm", "install", "-g", "--prefix", prefix, "bit-ship"
+    bin.install_symlink "bit-ship" => "bit-pod"
+    bin.install_symlink "bit-ship" => "bitship"
+    bin.install_symlink "bit-ship" => "bitpod"
   end
 
   test do
